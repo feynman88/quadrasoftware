9664a22eb4c7df95e1bbd3776bf2ef833ed2c130
Omar <44785357+feynman88@users.noreply.github.com>
2024-12-02 13:20:15 +0200

    added contact us page and tested

--- app/page.tsx
+++ app/page.tsx
@@ -86,7 +86,7 @@ export default function Home() {
                                             {/* // This is the call to action buttons of the hero section */}
                                             <div className="mt-10 flex items-center justify-center gap-x-6">
                                                 <a
-                                                    href="mailto:omar.assem.1913@gmail.com"
+                                                    href="/contact-us"
                                                     className="rounded-md bg-indigo-500 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-400 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-400"
                                                 >
                                                     تواصل معنا
